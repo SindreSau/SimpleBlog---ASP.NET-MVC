df4585e5aadb405c8e522a1f91766c4b23e8b6a8
SindreSau <sindre_50@hotmail.com>
2024-10-06 14:17:41 +0200

    Pagination working. No flashing on page load for light or dark-mode

--- SimpleBlog/Controllers/BlogPostController.cs
+++ SimpleBlog/Controllers/BlogPostController.cs
@@ -7,6 +7,7 @@ namespace SimpleBlog.Controllers
     public class BlogPostController : Controller
     {
         private readonly IBlogPostRepository _blogPostRepository;
+        private const int PageSize = 4; // Number of posts per page
 
         public BlogPostController(IBlogPostRepository blogPostRepository)
         {
@@ -15,12 +16,29 @@ namespace SimpleBlog.Controllers
 
         // GET: /
         [HttpGet]
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int page = 1)
         {
-            var blogPosts = await _blogPostRepository.GetPagedAsync(1, 2);
-            return View(blogPosts);
-        }
+            var totalPosts = await _blogPostRepository.GetCountAsync();
+            var totalPages = (int)Math.Ceiling((double)totalPosts / PageSize);
 
-        
+            var blogPosts = await _blogPostRepository.GetPagedAsync(page, PageSize);
+
+            var viewModel = new BlogPostIndexViewModel
+            {
+                BlogPosts = blogPosts,
+                CurrentPage = page,
+                TotalPages = totalPages
+            };
+
+            return View(viewModel);
+        }
     }
+}
+
+
+public class BlogPostIndexViewModel
+{
+    public IEnumerable<BlogPost> BlogPosts { get; set; }
+    public int CurrentPage { get; set; }
+    public int TotalPages { get; set; }
 }
